470d20ac0b3d10a0a07c0afa2537bf25e02372e0
unknown <bhhnguyen@gmail.com>
2020-06-04 19:44:44 -0500

    cleaning up

--- app.py
+++ app.py
@@ -17,15 +17,9 @@ app = Flask(__name__)
 #################################################
 
 from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
 
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-from .models import Pet
-
 
 # create route that renders index.html template
 @app.route("/")
@@ -37,13 +31,6 @@ def home():
 @app.route("/send", methods=["GET", "POST"])
 def send():
     if request.method == "POST":
-        name = request.form["petName"]
-        lat = request.form["petLat"]
-        lon = request.form["petLon"]
-
-        pet = Pet(name=name, lat=lat, lon=lon)
-        db.session.add(pet)
-        db.session.commit()
         return redirect("/", code=302)
 
     return render_template("form.html")
@@ -51,11 +38,6 @@ def send():
 
 @app.route("/api/pals")
 def pals():
-    results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
-
-    hover_text = [result[0] for result in results]
-    lat = [result[1] for result in results]
-    lon = [result[2] for result in results]
 
     pet_data = [{
         "type": "scattergeo",
