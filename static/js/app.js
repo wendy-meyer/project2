b2acbf550797ecbf122aa1f90374bcd2d7cac653
wendy-meyer <wendy.meyer.wm@gmail.com>
2020-06-11 20:32:30 -0500

    Added colors to app.js

--- static/js/app.js
+++ static/js/app.js
@@ -20,6 +20,23 @@ function graphit(yearSel, urlSel) {
     Plotly.d3.json(urlSel.concat(yearSel), (err, rows) => {
         //Filter the data by the dropdown selections
         const data = rows.filter(r => (r.party !== null));
+        var otherColors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+        '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+        '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+        '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+        '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+        '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+        '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+        '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+        '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+        '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF', 
+        '#8A2BE2', '#F0FFFF','#7FFFD4', '#5F9EA0', '#7FFF00' ];
+
+        var dict = {};
+        filteredParty2.forEach((party, i)=> {
+            dict[party] = otherColors[i];
+        }
+        )
 
         const trace = [{
             type: 'pie',
@@ -28,18 +45,19 @@ function graphit(yearSel, urlSel) {
             hovertext: data.map(d => d.candidate),
             showlegend: true,
             marker: {
-                'colors': [
-                    'red',
-                    'blue',
-                    'green',
-                    'yellow',
-                    'purple',
-                    'orange',
-                    'brown',
-                    'black',
-                    'grey',
-                    'pink'
-                ]
+                'colors':data.map(d => {
+                    if(d.party==="Democrat"){
+                        console.log("Demo!");
+                        return "blue"
+                    }
+                    else if (d.party==="Republican"){
+                        console.log("Rep!");
+                        return "red"
+                    }
+                    else {
+                        console.log("Other");
+                        return dict[d.party];
+                    }})
             }
         }]
 
